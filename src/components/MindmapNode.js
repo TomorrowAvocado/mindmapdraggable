a7647d47d858f16fe1c1afd90aaa3d64c9e4fb44
TomorrowAvocado <thomas.bugge@hiof.no>
2021-02-08 09:06:02 +0100

    Create node button moved.

--- src/components/MindmapNode.js
+++ src/components/MindmapNode.js
@@ -20,13 +20,13 @@ class MindmapNode extends Component {
                     width={this.state.nodeWidth + this.state.strokeWidth*2} /* Default width plus room for border */
                     height={this.state.nodeHeight + this.state.strokeWidth*2} /* Default height plus room for border */
                     >
+                        <button className="createNodeBtn">+</button>
                         <div className="mindMapNode" style={{
                             backgroundColor: this.state.fill,
                             borderStyle: "solid",
                             borderColor: this.state.strokeColor,
                             borderWidth: this.state.strokeWidth
                             }}>
-                            <button className="createNodeBtn">+</button>
                             <h2 onSelect={e => console.log("get text value")} contentEditable="true">{this.props.node.title}</h2>
                         </div>
                     </foreignObject>
