9a41f10d5201e57e20691a21149fe6d679ed8485
TomorrowAvocado <thomas.bugge@hiof.no>
2021-02-08 14:40:46 +0100

    Border-color on create-new-node-button

--- src/components/MindmapNode.js
+++ src/components/MindmapNode.js
@@ -13,26 +13,32 @@ class MindmapNode extends Component {
         return (
             <Draggable cancel="h2">
                 <g>
-                    <foreignObject x="40vw" y="40vh" 
-                    width={this.props.node.nodeWidth + this.props.node.strokeWidth*2} /* Default width plus room for border */
-                    height={this.props.node.nodeHeight + this.props.node.strokeWidth*2} /* Default height plus room for border */
+                    <foreignObject x="40vw" y="40vh"
+                        width={this.props.node.nodeWidth + this.props.node.strokeWidth * 2} /* Default width plus room for border */
+                        height={this.props.node.nodeHeight + this.props.node.strokeWidth * 2} /* Default height plus room for border */
                     >
-                        <div onMouseEnter={e => this.setState({buttonVisible: true})} onMouseLeave={e => this.setState({buttonVisible: false})}>
-                            <button style={this.state.buttonVisible ? {visibility: "visible"} : {visibility: "hidden"} } className="createNodeBtn" onClick={this.props.createNewNode}>+</button>
-                            <div 
-                            onClick={this.props.handleSelected.bind(this, this.props.node.id)} 
-                            className="mindMapNode" 
-                            style={this.props.node.isSelected ? {
-                                borderColor:"blue",
-                                backgroundColor: this.props.node.fill,
-                                } : {
+                        <div onMouseEnter={e => this.setState({ buttonVisible: true })} onMouseLeave={e => this.setState({ buttonVisible: false })}>
+                            <button
+                                style={{
+                                    visibility: this.state.buttonVisible ? "visible" : "hidden",
+                                    borderColor: this.props.node.strokeColor
+                                }}
+                                className="createNodeBtn"
+                                onClick={this.props.createNewNode}>+</button>
+                            <div
+                                onClick={this.props.handleSelected.bind(this, this.props.node.id)}
+                                className="mindMapNode"
+                                style={this.props.node.isSelected ? {
+                                    borderColor: "blue",
                                     backgroundColor: this.props.node.fill,
-                                    borderStyle: "solid",
-                                    borderColor: this.props.node.strokeColor,
-                                    borderWidth: this.props.node.strokeWidth
+                                } : {
+                                        backgroundColor: this.props.node.fill,
+                                        borderStyle: "solid",
+                                        borderColor: this.props.node.strokeColor,
+                                        borderWidth: this.props.node.strokeWidth
+                                    }
                                 }
-                            }
-                                >
+                            >
                                 <h2 onClick={e => console.log("get text value")} contentEditable="true">{this.props.node.title}</h2>
                             </div>
                         </div>
