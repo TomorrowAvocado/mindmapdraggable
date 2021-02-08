6718ea3de801fd19ae8864c77e41e5c689dce558
TomorrowAvocado <thomas.bugge@hiof.no>
2021-02-08 09:50:56 +0100

    Refactor: moved node state.

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -2,15 +2,39 @@ import React, { Component } from 'react'
 import { useState } from "react";
 import MindmapNode from './MindmapNode'
 
-const nodes = [{ title: "node one" }, { title: "node two" }]
+
 class Canvas extends Component {
+
+state = { nodes: [{ 
+    title: "node one",
+    nodeWidth: 200,
+    nodeHeight: 150,
+    strokeColor: "green",
+    strokeWidth: 3,
+    fill: "white",
+}, 
+    { 
+    title: "node two",
+    nodeWidth: 200,
+    nodeHeight: 150,
+    strokeColor: "green",
+    strokeWidth: 3,
+    fill: "white",
+ },
+]}
+
+createNewNode = () => {
+    const newNode = {title: "new node"}
+    this.setState({nodes: [...this.state.nodes, newNode]})
+    console.log()
+};
 
     render() {
         return (
             <svg width="100vw" height="100vh" viewBox="0 0 100vw 100vh">
-                {nodes.map(node =>
+                {this.state.nodes.map(node =>
                     <svg>
-                        <MindmapNode node={node} />
+                        <MindmapNode node={node} createNewNode={this.createNewNode} />
                     </svg>
                 )}
             </svg>
--- src/components/MindmapNode.js
+++ src/components/MindmapNode.js
@@ -3,29 +3,22 @@ import React, { Component } from 'react'
 import Draggable from 'react-draggable';
 
 class MindmapNode extends Component {
-    state = {
-        nodeWidth: 200,
-        nodeHeight: 150,
-        strokeColor: "green",
-        strokeWidth: 3,
-        fill: "white",
-    }
 
     render() {
 
         return (
             <Draggable cancel="h2">
                 <g>
-                    <foreignObject x="0" y="0" 
-                    width={this.state.nodeWidth + this.state.strokeWidth*2} /* Default width plus room for border */
-                    height={this.state.nodeHeight + this.state.strokeWidth*2} /* Default height plus room for border */
+                    <foreignObject x="40vw" y="40vh" 
+                    width={this.props.node.nodeWidth + this.props.node.strokeWidth*2} /* Default width plus room for border */
+                    height={this.props.node.nodeHeight + this.props.node.strokeWidth*2} /* Default height plus room for border */
                     >
-                        <button className="createNodeBtn">+</button>
+                        <button className="createNodeBtn" onSubmit={this.props.createNewNode}>+</button>
                         <div className="mindMapNode" style={{
-                            backgroundColor: this.state.fill,
+                            backgroundColor: this.props.node.fill,
                             borderStyle: "solid",
-                            borderColor: this.state.strokeColor,
-                            borderWidth: this.state.strokeWidth
+                            borderColor: this.props.node.strokeColor,
+                            borderWidth: this.props.node.strokeWidth
                             }}>
                             <h2 onSelect={e => console.log("get text value")} contentEditable="true">{this.props.node.title}</h2>
                         </div>
@@ -35,7 +28,5 @@ class MindmapNode extends Component {
         )
     }
 }
-
-
 
 export default MindmapNode;
