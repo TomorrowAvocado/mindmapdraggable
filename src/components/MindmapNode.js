c214d11034777ebef5cf479942c1978cb49f6d22
TomorrowAvocado <thomas.bugge@hiof.no>
2021-02-24 10:58:30 +0100

    Removed unused parameter.

--- src/components/MindmapNode.js
+++ src/components/MindmapNode.js
@@ -25,6 +25,7 @@ class MindmapNode extends Component {
 
     createNewNode(x, y) {
         const newNode = {
+            id: getId(),
             x: x,
             y: y,
             children: []
@@ -33,7 +34,7 @@ class MindmapNode extends Component {
     }
 
     handlePlusBtnClick() {
-        const newNode = this.createNewNode(this.state.x + 100, 0)
+        const newNode = this.createNewNode(this.state.x + 100)
         // Add new node to children in state
         this.setState({
             children: [...this.state.children, newNode]
