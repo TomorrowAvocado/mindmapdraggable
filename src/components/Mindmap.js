d68311fc0ec9c34779a06cfa18688a48457cfdc2
magnusnorvik <43719819+magnusnorvik@users.noreply.github.com>
2021-03-24 12:32:05 +0100

    PanZoomTested. Limited progress.

--- src/components/Mindmap.js
+++ src/components/Mindmap.js
@@ -1,44 +1,53 @@
-import React, { Component } from 'react'
+import React, { Component, useState, useRef, useEffect } from 'react'
 import MindmapNode from './MindmapNode';
+import * as d3 from 'd3'
 
-export default class Mindmap extends Component {
+const Mindmap = () => {
 
-    constructor() {
-        super()
-
-        this.state = {
-            id: "Some UUID",
-            title: "MY MINDMAP!!",
-            mainNode: {
-                id: "Eve",
-                x: 300,
-                y: 300,
-                layout: "mindmap",
-                text: "Main node",
-                children: [
-                    {
-                        id: "EveChild",
-                        x: 450,
-                        y: 450,
-                        layout: "mindmap",
-                        text: "1st level node",
-                        children: [
-                            {
-                                id: "EveGrandChild",
-                                x: 600,
-                                y: 600,
-                                layout: "mindmap",
-                                text: "2nd level node",
-                                children: []
-                            }
-                        ]
-                    }
-                ]
-            }
+    const [nodes, setNodes] = useState({
+        id: "Some UUID",
+        title: "MY MINDMAP!!",
+        mainNode: {
+            id: "Eve",
+            x: 300,
+            y: 300,
+            layout: "mindmap",
+            text: "Main node",
+            children: [
+                {
+                    id: "EveChild",
+                    x: 450,
+                    y: 450,
+                    layout: "mindmap",
+                    text: "1st level node",
+                    children: [
+                        {
+                            id: "EveGrandChild",
+                            x: 600,
+                            y: 600,
+                            layout: "mindmap",
+                            text: "2nd level node",
+                            children: []
+                        }
+                    ]
+                }
+            ]
         }
-    }
+    })
 
-    updateMainNode(node, index) {
+    const [tool, setTool] = useState(TOOL_AUTO)
+
+    const Viewer = useRef(null);
+
+    useEffect(() => {
+        Viewer.current.fitToViewer();
+    }, []);
+
+    const _zoomOnViewerCenter = () => Viewer.current.zoomOnViewerCenter(1.1)
+    const _fitSelection = () => Viewer.current.fitSelection(40, 40, 200, 200)
+    const _fitToViewer = () => Viewer.current.fitToViewer()
+
+    const updateMainNode = (node, index) => {
         // This method gets called from the MindMapNode component
         // The MindMapNode will send its index. The index indicates its placement in the children list. 
         // In this case, the index is whatever we sent as a prop from here.
@@ -50,16 +59,16 @@ export default class Mindmap extends Component {
         })
     }
 
-    render() {
-        return (
-            <svg width="1000" height="800">
-                <MindmapNode 
-                    node={this.state.mainNode} 
-                    parentX={this.state.mainNode.x} 
-                    parentY={this.state.mainNode.y}
-                    addMeToMyParent={this.updateMainNode.bind(this)}
-                    index={0} />
-            </svg>
-        )
-    }
+    return (
+        <svg width="1000" height="800" style={{backgroundColor: "white"}}>
+            <MindmapNode 
+                node={nodes.mainNode} 
+                parentX={nodes.mainNode.x} 
+                parentY={nodes.mainNode.y}
+                addMeToMyParent={updateMainNode.bind(this)}
+                index={0} />
+        </svg>
+    )
 }
+
+export default Mindmap;
